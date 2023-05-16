207ee396331265de0759bcd9a2dccb88ed2e2bc9
calvin-arieri <calvin.arieri@student.moringaschoool.com>
2023-05-16 09:17:48 +0300

    tried my bes

--- Day1/dayOne.py
+++ Day1/dayOne.py
@@ -6,3 +6,29 @@
 
 # #### Exercise
 # Simple Python Calculator Program: Write a simple program that does basic math operations including addition, subtraction, deletion, division, power, squareroot,etc,. 
+number_1 = int(input("enter a number: "))
+number_2 = int(input("enter second number: "))
+
+def sumFunction():
+    print("sum of numbers", number_1 + number_2)
+
+
+def differenceFunction():
+    print("subtraction of numbers", number_1 - number_2)
+
+def divisionFunction():
+    print("divide two numbers", int(number_1/number_2))
+
+def powerFunction():
+    print("powering the two numbers", pow(number_1, number_2))
+
+def deletionFunction():
+    number_1 = 0
+    number_2 = 0
+    print("number 1 deleted", number_1)
+    print("number 2 deleted", number_2)
+powerFunction()                 
+divisionFunction()    
+sumFunction()
+differenceFunction()  
+deletionFunction()  
