21d8a2f790f0232bace1087866d5ff2c3ea57fc5
calvin-arieri <calvin.arieri@student.moringaschoool.com>
2023-05-21 01:06:23 +0300

    been lazy but catching up

---
+++ Day4/day4.py
@@ -0,0 +1,21 @@
+def listToBeSummed():
+    num  = list(range(11))
+    sumOfNumbers = 0
+
+    for number in num:
+        sumOfNumbers = sumOfNumbers + number
+    print(sumOfNumbers)   
+listToBeSummed()
+
+def findSecondLargest():
+    num  = list(range(11))
+    sumOfNumbers = 0
+
+    for number in num:
+        sumOfNumbers = sumOfNumbers + number
+    print(sumOfNumbers)  
+findSecondLargest() 
+
+def personIntroduction(name, age, occupation):
+    print(f"My name is {name}, I am {age} years old and I work as a {occupation}.")
+personIntroduction("calvin Arieri", "19", "full stack software engineer" )   
